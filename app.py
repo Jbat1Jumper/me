acbed9a055168cac3d5acc9381efb3d05406e8f6
Nikita Zdanovitch <nzdanovitch@gmail.com>
2016-09-16 01:51:09 -0300

    notebook support

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from nbconvert import export_html
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return export_html('Jbat1Jumper\'s Website.ipnb')[0]
+
+@app.route("/<notebook>")
+def page(notebook):
+    file = 'notebooks/{}.ipynb'.format(notebook.replace('_', ' '))
+    return export_html(file)[0]
 
 if __name__ == "__main__":
     app.run()
